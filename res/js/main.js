6961ae7dcedfc1306ba8ff452cc97f9f7115f71a
tade18 <tade.18@seznam.cz>
2024-01-17 09:43:48 +0100

    v2

--- res/js/main.js
+++ res/js/main.js
@@ -1,7 +1,10 @@
 const changeButton = document.getElementById("changeButton");
-const hexdec = document.getElementById("hexdec")
+const hexdec = document.getElementById("hexdec");
+const inputColor = document.getElementById("inputColor");
 var clickSound = new Audio('../res/audio/clicksound.mp3');
 const body = document.body;
+
+//VYTVOŘENÍ NÁHODNÉ BARVA
 function getRandomColor(){
     var letters = "0123456789ABCDEF";
     var color = "#";
@@ -10,22 +13,44 @@ function getRandomColor(){
     }
     return color;
 }
+//ZMĚNÍ BARVU NA RANDOM
 function changeBackgroundColor() {
     var randomColor = getRandomColor();
     body.style.backgroundColor = randomColor;
     hexdec.innerText = randomColor;
 }
-changeButton.onclick=()=> {
-    hideElement(changeButton);
+//BARVA PŘEVZATÁ Z USER INPUT
+function customBackgroundColor() {
+    var userColor = inputColor.value;
+    body.style.backgroundColor = userColor;
+    hexdec.innerText = userColor;
+}
+
+//POUŽITÍ TLAČÍTKA PRO ZMĚNU BARVY
+changeButton.onclick = () =>{
+    if (inputColor.value=="") {
+        hideElement(changeButton);
     setTimeout(() => {
         showElement(changeButton)
     }, 1000);
     changeBackgroundColor();
     clickSound.play();
+    }
+    else{
+        hideElement(changeButton);
+    setTimeout(() => {
+    showElement(changeButton)
+    }, 1000);
+    customBackgroundColor();
+    clickSound.play();
+    }
 }
+
+//HIDE ELEMENT
 const hideElement = (element) => {
     element.style.display = "none";
 }
+//SHOW ELEMENT
 const showElement = (element) => {
     element.style.display = "initial"
 }
